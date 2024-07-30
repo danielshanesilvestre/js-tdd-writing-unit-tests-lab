8d6581862bd594bd729447f42c536f90fc817134
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-07-30 10:15:30 -0500

    Completed lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,26 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("accepts a string and returns a boolean for whether the string is a palindrome", () => {
+    const word = "racecar";
+
+    const result = isPalindrome(word);
+
+    expect(result).toBe(true);
+  });
+  it("returns true for palindromes wiht uppercase letters", () => {
+    const word = "rAcEcAr";
+
+    const result = isPalindrome(word);
+
+    expect(result).toBe(true);
+  });
+  it("returns false for an empty string", () => {
+    const word = "";
+
+    const result = isPalindrome(word);
+
+    expect(result).toBe(false);
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,18 @@
 // Your code here
+
+export function isPalindrome(word) {
+  if (word.length === 0)
+    return false;
+
+  let end = (word.length - (word.length % 2)) / 2;
+
+  let lowercaseWord = word.toLowerCase();
+
+  for (let i = 0; i < end; i++) {
+    if (lowercaseWord[i] !== lowercaseWord[word.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
